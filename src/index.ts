1ebd5fb6e2554ed63e6feb271b0f74c16001aa59
Adrian Kosmaczewski <adrian@vshn.ch>
2019-07-02 08:19:24 +0200

    Added documentation

--- src/index.ts
+++ src/index.ts
@@ -7,49 +7,72 @@ import path from 'path'
 import fs from 'fs'
 import lunr from 'lunr'
 
-type FileReference = { [ref: string]: SearchResult }
+type FileRepository = { [ref: string]: SearchResult }
 
-const files = getConfig('files.json') as FileReference
-const lunrIndexSource = getConfig('lunr.json')
-const lunrIndex = lunr.Index.load(lunrIndexSource)
-
+/**
+ * Returns the contents of the JSON file whose name is passed as parameter.
+ * @param file A valid filename
+ */
 function readJsonFileSync(filepath: string, encoding = 'utf8'): object {
+  if (!fs.existsSync(filepath)) {
+    throw `File "${filepath}" does not exist'`
+  }
   const file = fs.readFileSync(filepath, encoding)
   return JSON.parse(file)
 }
 
-function getConfig(file: string): object {
+/**
+ * Returns a JSON object with the contents of a configuration file.
+ * @param file A valid filename
+ */
+function getConfigSync(file: string): object {
   const filepath = path.join(__dirname, '..', 'index', file)
   return readJsonFileSync(filepath)
 }
 
+/**
+ * Returns true if a string is null, undefined, empty or blank after trimming.
+ * @param s The string to test
+ */
 function isEmptyOrBlank(s: string): boolean {
   return (!s || s.length === 0 || !s.trim())
 }
 
-function search(query: string): SearchResult[] {
+/**
+* Performs the actual search against the Lunr index.
+ * @param lunrIndex The Lunr.js index
+ * @param files The repository of files
+ * @param query Text to search
+ * @param count Number of items to return
+ */
+function search(lunrIndex: lunr.Index, files: FileRepository, query: string, count = 10): SearchResult[] {
   if (isEmptyOrBlank(query)) {
     return []
   }
-  // Search with Lunr.js, but return at most 10 items.
   const results: SearchResult[] = lunrIndex.search(query.trim())
-                                           .slice(0, 10)
-                                           .map(function (result: lunr.Index.Result) {
-    return files[result.ref]
-  })
+    .slice(0, count)
+    .map(function (result: lunr.Index.Result) {
+      return files[result.ref]
+    })
   return results
 }
 
-// Entry point
+// Entry point of the application
 try {
   const app = express()
+
+  // These methods are synchronous, since the index is read once and held in memory
+  const files = getConfigSync('files.json') as FileRepository
+  const lunrIndexSource = getConfigSync('lunr.json')
+  const lunrIndex = lunr.Index.load(lunrIndexSource)
+  const port = 3000
+
   app.get('/search', (req: Request, res: Response) => {
-    res.send(search(req.query.q))
+    res.send(search(lunrIndex, files, req.query.q))
   })
 
-  const port = 3000
   app.listen(port, () => console.log(`Search engine listening on port ${port}`))
 }
 catch (error) {
-  console.error('Application error: ' + error)
+  console.error(`Application error: ${error}`)
 }
